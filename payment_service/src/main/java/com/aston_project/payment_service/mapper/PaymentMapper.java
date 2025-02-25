af0842ec80ab4c2ca23c37eec77d41f9c0d6bbac
fuckstrout <Fuckstrout@mail.ru>
2025-02-26 01:08:49 +0300

    Update PaymentMapper.java

--- payment_service/src/main/java/com/aston_project/payment_service/mapper/PaymentMapper.java
+++ payment_service/src/main/java/com/aston_project/payment_service/mapper/PaymentMapper.java
@@ -6,22 +6,15 @@ import com.aston_project.payment_service.entity.Payment;
 public class PaymentMapper {
 
     public static PaymentDto toDTO(Payment payment) {
-        PaymentDto paymentDTO = new PaymentDto();
-        paymentDTO.setId(payment.getId());
-        paymentDTO.setOrderId(payment.getOrderId());
-        paymentDTO.setStatus(payment.getStatus());
-        paymentDTO.setSum(payment.getSum());
-        paymentDTO.setPrice(payment.getPrice());
-        return paymentDTO;
+        return new PaymentDto(payment.getId(), payment.getOrderId(), payment.getSum(), payment.getStatus());
     }
 
     public static Payment toEntity(PaymentDto paymentDTO) {
         Payment payment = new Payment();
         payment.setId(paymentDTO.getId());
         payment.setOrderId(paymentDTO.getOrderId());
-        payment.setStatus(paymentDTO.getStatus());
         payment.setSum(paymentDTO.getSum());
-        payment.setPrice(paymentDTO.getPrice());
+        payment.setStatus(paymentDTO.getStatus());
         return payment;
     }
 }
